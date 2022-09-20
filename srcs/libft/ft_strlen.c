2963db3144bab57bb2fb702291cf4d434e6b65ce
benatcastro <benatcastro@gmail.com>
2022-09-20 17:13:28 +0200

    libft extended compiling

--- srcs/libft/ft_strlen.c
+++ srcs/libft/ft_strlen.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strlen.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aalvarez <aalvarez@student.42.fr>          +#+  +:+       +#+        */
+/*   By: becastro <becastro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/16 19:00:30 by aalvarez          #+#    #+#             */
-/*   Updated: 2022/08/17 23:30:48 by aalvarez         ###   ########.fr       */
+/*   Updated: 2022/09/20 17:08:25 by becastro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 /**
  * @brief takes a string and returns its size (without counting
  * the final NULL byte).
- * 
+ *
  * @param str the string to be counted.
  * @return size_t the size of the string.
  */
@@ -23,8 +23,6 @@ size_t	ft_strlen(const char *str)
 {
 	size_t	i;
 
-	if (!str)
-		return (0);
 	i = 0;
 	while (str[i])
 		i++;
