4f0eab77a8a96df253d22927fc2a5fed64292f31
benatcastro <benatcastro@gmail.com>
2022-09-29 16:28:56 +0200

    rebase expander

--- srcs/parser/parser_core.c
+++ srcs/parser/parser_core.c
@@ -6,13 +6,13 @@
 /*   By: becastro <becastro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/27 16:20:38 by umartin-          #+#    #+#             */
-/*   Updated: 2022/09/29 14:02:14 by becastro         ###   ########.fr       */
+/*   Updated: 2022/09/29 16:28:25 by becastro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../../includes/minishell.h"
-#include "../../includes/parser.h"
-#include "expander.h"
+#include "minishell.h"
+#include "parser.h"
+#include "lexer.h"
 
 char	**parser_core(char **str)
 {
@@ -30,35 +30,39 @@ char	**parser_core(char **str)
 
 char	*parser_quotes(char *str)
 {
-	int		a;
+	int		i;
 	int		c;
-	int		b;
-	// int		n;
+	int		checker;
 	char	*rtn;
 
-	a = 0;
+	i = 0;
 	c = 0;
-	rtn = NULL;
-
-	// rtn = malloc(sizeof(char *) * ft_strlen(str) - 2);
-	// printf("%s\n", str);
-	// while (str[a])
-	// {
-	// 	if (str[a] == 39)
-	// 	{
-	// 		while (str[++a] != 39)
-	// 			rtn[a] = rtn[c++];
-	// 		if ((size_t)a != ft_strlen(str))
-	// 			a++;
-	// 		else
-	// 			break ;
-	// 	}
-	// 	rtn[a] = rtn[c++];
-	// 	a++;
-	// }
-	// b = 0;
-	b = parser_quote_memory(str);
-	printf ("%i\n", b);
+	printf ("%i\n", parser_quote_memory(str));
+	rtn = ft_calloc(sizeof(char *), (ft_strlen(str) - parser_quote_memory(str)) + 1);
+	while (str[i])
+	{
+		if (str[i] == 39 || str[i] == 34)
+		{
+			if (str[i] == 39)
+				checker = 1;
+			else
+				checker = 2;
+			i++;
+			if ((size_t)i + 1 == ft_strlen(str))
+				break ;
+			if (checker == 1)
+			{
+				while ((str[i] != 39) && ((size_t)i != ft_strlen(str)))
+					rtn[c++] = str[i++];
+			}
+			else
+			{
+				while ((str[i] != 34) && ((size_t)i != ft_strlen(str)))
+					rtn[c++] = str[i++];
+			}
+		}
+		rtn[c++] = str[i++];
+	}
 	return (rtn);
 }
 
@@ -81,35 +85,39 @@ int	parser_quote_memory(char *str)
 				checker = 2;
 			b = 1;
 			n++;
-			i++;
 			if ((size_t)i + 1 == ft_strlen(str))
 				break ;
+			i++;
 			if (checker == 1)
 			{
-				while ((str[i] != 39) && ((size_t)i != ft_strlen(str)))
+				while ((size_t)i != ft_strlen(str))
 				{
-					i++;
 					if (str[i] == 39)
 					{
 						b = 0;
 						n++;
+						break ;
 					}
+					i++;
 				}
 			}
 			else
 			{
-				while ((str[i] != 34) && ((size_t)i != ft_strlen(str)))
+				while ((size_t)i != ft_strlen(str))
 				{
-					i++;
 					if (str[i] == 34)
 					{
 						b = 0;
 						n++;
+						break ;
 					}
+					i++;
 				}
 			}
 		}
 		i++;
 	}
+	if (b == 1)
+		return (-1);
 	return (n);
 }
