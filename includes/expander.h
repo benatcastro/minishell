aa102a51d86295b5e9cdfd2216759a3808c37721
PILTRAFILLA317 <unaimartinm@gmail.com>
2022-10-04 21:16:15 +0200

    casi el expander

--- includes/expander.h
+++ includes/expander.h
@@ -6,7 +6,7 @@
 /*   By: umartin- <umartin-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/29 12:34:14 by becastro          #+#    #+#             */
-/*   Updated: 2022/09/30 18:52:59 by umartin-         ###   ########.fr       */
+/*   Updated: 2022/10/04 21:01:53 by umartin-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,13 @@
 
 char	*expander_core(char **cmds, char **env);
 char	*expander_main(char	*str, char **env);
-char	**expand_splitter(char *str);
+char	**expand_splitter(char *str, char **rtn);
 int		expand_arg_num(char *str);
 void	expand_arg_num_ut(char *str, int *i, int *n);
 void	exander_dollar_find(char *str, char **temp, int *i, int *n);
 void	expaner_no_dollar(char *str, char **temp, int *i, int *n);
-char	*expander(char	*str, char **env);
+char	*expand_first_trimmer(char	*str);
+char	**expander(char	*str, char **env);
 char	*env_replacer(char *str, char **env);
 int		num_until_equal(char *str);
 char	*expand_trimmer(char	*str);
--- srcs/expander/expander_core.c
+++ srcs/expander/expander_core.c
@@ -6,7 +6,7 @@
 /*   By: umartin- <umartin-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/29 12:31:37 by becastro          #+#    #+#             */
-/*   Updated: 2022/10/01 17:41:20 by umartin-         ###   ########.fr       */
+/*   Updated: 2022/10/04 21:15:05 by umartin-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,6 +55,24 @@ char	*expand_trimmer(char	*str)
 	return (rtn);
 }
 
+char	*expand_first_trimmer(char	*str)
+{
+	int		i;
+	int		c;
+	char	*rtn;
+
+	i = 0;
+	c = 0;
+	rtn = ft_calloc(sizeof(char *), (ft_strlen(str) - 2));
+	while (++i < (ft_strlen(str)))
+	{
+		rtn[c] = str[i];
+		c++;
+	}
+	rtn[i] = 0;
+	return (rtn);
+}
+
 char	*expander_main(char	*str, char **env)
 {
 	char	*rtn;
@@ -72,60 +90,80 @@ char	*expander_main(char	*str, char **env)
 	return (rtn);
 }
 
-char	*expander(char	*str, char **env)
+char	**expander(char	*str, char **env)
 {
 	int		i;
 	int		n;
 	char	**temp;
+	char	*aux;
 
 	temp = malloc(sizeof(char *) * (expand_arg_num(str)) + 1);
-	temp = expand_splitter(str);
+	temp = expand_splitter(str, temp);
+	i = -1;
+	while (temp[++i])
+		printf("%s\n", temp[i]);
+	printf("\n");
 	i = -1;
 	n = 0;
-	while (++i <= ((int)ft_strlen(str) - 1))
-	{
-		if (str[i] == DOUBLE_QUOTE)
-			parser_double_q(str, temp, &i, &n);
-		else if (str[i] == SINGLE_QUOTE)
-			parser_single_q(str, temp, &i, &n);
-		else
-			parser_no_q(str, temp, &i, &n);
-	}
+	while (temp[++i])
+		if (temp[i][0] == 36)
+		{
+			aux = env_replacer(temp[i], env);
+			printf("%s\n", aux);
+			temp[i] = ft_strdup(aux);
+		}
 	return (temp);
+}
+
+char	*env_replacer(char *str, char **env)
+{
+	int		i;
+	int		n;
+	int		r;
+	char	*val;
+	char	*p;
+
+	i = -1;
+	str = expand_first_trimmer(str);
+	p = ft_itoa((int)getpid());
+	if (str[1] == 36)
+		return (p);
+	while (env[++i])
+	{
+		if ((!ft_strncmp(str, env[i], (num_until_equal(env[i]))))
+			&& (ft_strlen(str) == num_until_equal(env[i])))
+		{
+			val = ft_calloc(sizeof(char *),
+					(ft_strlen(env[i]) - ft_strlen(str)));
+			n = (ft_strlen(str));
+			r = 0;
+			while (env[i][++n])
+				val[r++] = env[i][n];
+			val[r] = 0;
+			return (val);
+		}
+	}
+	val = 0;
+	return (val);
+}
+
+int	num_until_equal(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == 61)
+			return (i);
+		i++;
+	}
+	return (0);
 }
 
 int	expand_arg_num(char *str)
 {
 	int		e;
-	int		c;
-
-	e = -1;
-	c = 0;
-	while (str[++e])
-	{
-		if (str[e] == 36)
-		{
-			c++;
-			e += 2;
-			while ((str[e]) && ((str[e] != '$') && (str[e] != 32)))
-				e++;
-			e--;
-		}
-		else
-		{
-			c++;
-			while ((str[e]) && ((str[e] != '$') && (str[e] != 32)))
-				e++;
-			e--;
-		}
-	}
-	return (c);
-}
-
-char	**expand_splitter(char *str)
-{
-	int		e;
-	int		l;
 	int		c;
 
 	e = 0;
@@ -134,20 +172,85 @@ char	**expand_splitter(char *str)
 	{
 		if (str[e] == 36)
 		{
-			l = 0;
-			e += 2;
+			if (str[e + 1] == 36)
+			{
+				c++;
+				e += 2;
+				continue ;
+			}
+			c++;
+			e++;
 			while ((str[e]) && ((str[e] != '$') && (str[e] != 32)))
 				e++;
 			e--;
 		}
 		else
 		{
-			l = 0;
-			while ((str[e]) && ((str[e] != '$') && (str[e] != 32)))
+			c++;
+			while ((str[e]) && ((str[e] != '$')))
 				e++;
-			e--;
 		}
 		e++;
 	}
 	return (c);
+}
+
+char	**expand_splitter(char *str, char **rtn)
+{
+	int		e;
+	int		c;
+	int		n;
+	int		l;
+	int		a;
+	char	*aux;
+
+	e = 0;
+	c = 0;
+	n = 0;
+	while (str[e])
+	{
+		if (str[e] == 36)
+		{
+			l = e;
+			if (str[e + 1] == 36)
+			{
+				c++;
+				e++;
+			}
+			else
+			{
+				c++;
+				e++;
+				while ((str[e]) && ((str[e] != '$') && (str[e] != 32)))
+					e++;
+			}
+			aux = ft_calloc(sizeof(char *), (e - l) + 1);
+			a = 0;
+			while (l <= e)
+				aux[a++] = str[l++];
+			aux[l] = 0;
+			rtn[n] = ft_strdup(aux);
+			free(aux);
+			n++;
+		}
+		else
+		{
+			l = e;
+			c++;
+			while ((str[e]) && ((str[e] != '$')))
+				e++;
+			e--;
+			aux = ft_calloc(sizeof(char *), (e - l) + 1);
+			a = 0;
+			while (l <= e)
+				aux[a++] = str[l++];
+			aux[l] = 0;
+			rtn[n] = ft_strdup(aux);
+			free(aux);
+			n++;
+		}
+		e++;
+	}
+	rtn[n] = NULL;
+	return (rtn);
 }
