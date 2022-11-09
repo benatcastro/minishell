487462fddc54ce5fdbfd9c0868c21d53dd19c5ca
benatcastro <benatcastro@gmail.com>
2022-11-09 16:09:04 +0100

    signal handler fix

--- srcs/signals/signal_handler.c
+++ srcs/signals/signal_handler.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   signal_handler.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: umartin- <umartin-@student.42.fr>          +#+  +:+       +#+        */
+/*   By: becastro <becastro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/28 16:28:22 by becastro          #+#    #+#             */
-/*   Updated: 2022/11/05 01:38:53 by umartin-         ###   ########.fr       */
+/*   Updated: 2022/11/09 15:32:20 by becastro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,10 @@ void	signal_reciever(int signal, siginfo_t *data, void *ucontext)
 {
 	(void)ucontext;
 	(void)data;
+	printf("\n%s", PROMPT);
 	//rintf("recieved signal (%d)\n", signal);
-	printf ("\n");
-	rl_on_new_line();
+	// printf ("\n");
+	// rl_on_new_line();
 	//rl_replace_line();
-	rl_redisplay();
+	// rl_redisplay();
 }
