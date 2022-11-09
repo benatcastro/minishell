65621de82cb90a4a5a1d817e3bcaeb596d182f93
benatcastro <benatcastro@gmail.com>
2022-11-09 19:27:46 +0100

    signals fncs

--- srcs/signals/signal_handler.c
+++ srcs/signals/signal_handler.c
@@ -6,16 +6,20 @@
 /*   By: becastro <becastro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/28 16:28:22 by becastro          #+#    #+#             */
-/*   Updated: 2022/11/09 16:34:58 by becastro         ###   ########.fr       */
+/*   Updated: 2022/11/09 19:14:38 by becastro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "signals.h"
 #include <stdio.h>
 #include "minishell.h"
+#include <signal.h>
 
-void	signal_reciever(int signal, siginfo_t *data, void *ucontext)
+void	signal_reciever(int signal, siginfo_t *data, void *pid_ptr)
 {
-	(void)ucontext;
-	printf("\n%s", PROMPT);
+	// printf ("pid (%d)\n", data->si_pid);
+	// if (signal == SIGINT)
+		// printf("\n%s", PROMPT);
+	if (signal == SIGQUIT)
+		exit(EXIT_SUCCESS);
 }
--- srcs/signals/signals_core.c
+++ srcs/signals/signals_core.c
@@ -3,24 +3,38 @@
 /*                                                        :::      ::::::::   */
 /*   signals_core.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: umartin- <umartin-@student.42.fr>          +#+  +:+       +#+        */
+/*   By: becastro <becastro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/28 16:24:20 by becastro          #+#    #+#             */
-/*   Updated: 2022/11/05 01:35:46 by umartin-         ###   ########.fr       */
+/*   Updated: 2022/11/09 19:03:53 by becastro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "signals.h"
 #include "minishell.h"
 
+int	real_pid(void)
+{
+	int		s;
+	int		pid;
+
+	pid = fork();
+	if (pid == 0)
+		exit (0);
+	else
+		s = pid;
+	return (s - 1);
+}
+
 int	signals_core(void)
 {
 	struct sigaction	sig_data;
+	struct sigaction	pid;
 
 	sig_data.sa_sigaction = signal_reciever;
-	sigemptyset (&sig_data.sa_mask);
-	sig_data.sa_flags = SA_SIGINFO;
-	sigaction(SIGINT, &sig_data, NULL);
-	sigaction(SIGQUIT, &sig_data, NULL);
-	return (1);
+	sigemptyset (&sig_data.sa_flags);
+	sig_data.sa_mask = SA_SIGINFO;
+	sigaction(SIGINT, &sig_data, &pid);
+	sigaction(SIGQUIT, &sig_data, &pid);
+	return (pid.sa_flags);
 }
