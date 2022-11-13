9881e7818a17e973caee268cb046e8941c133053
benatcastro <benatcastro@gmail.com>
2022-11-13 05:36:04 +0100

    signal flags OK

--- srcs/signals/signal_handler.c
+++ srcs/signals/signal_handler.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   signal_handler.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bena <bena@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: becastro <becastro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/28 16:28:22 by becastro          #+#    #+#             */
-/*   Updated: 2022/11/12 14:49:28 by bena             ###   ########.fr       */
+/*   Updated: 2022/11/13 05:35:46 by becastro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,8 @@
 
 void	signal_reciever(int signal, siginfo_t *data, void *ucontext)
 {
-	extern t_data	g_data;
-
-	// printf("ms pid %d handle %d", g_data.ms_pid, data->si_pid);
+	(void)data;
+	(void)ucontext;
 	if (signal == SIGINT && !g_data.sub_pid)
 	{
 		printf("\n");
