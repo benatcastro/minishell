33a7708c8a3f155d363c239712286d0d392536f7
benatcastro <benatcastro@gmail.com>
2022-11-13 12:03:20 +0100

    cd working

--- srcs/builtins/cd.c
+++ srcs/builtins/cd.c
@@ -6,7 +6,7 @@
 /*   By: becastro <becastro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/09 05:27:26 by becastro          #+#    #+#             */
-/*   Updated: 2022/11/13 06:22:38 by becastro         ###   ########.fr       */
+/*   Updated: 2022/11/13 12:02:47 by becastro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,6 +33,8 @@ static int	check_dir(char *path)
 	int	val;
 
 	val = valid_path(path);
+	if (ft_strcmp(path, "-") && !find_in_env(path))
+		return (printf("%scd: OLDPWD not set\n", PROMPT), 0);
 	if (val == 0)
 		return ((printf("%scd: %s: No such file or directory\n",
 					PROMPT, path)), 0);
@@ -47,9 +49,7 @@ void	cd_builtin(char **args)
 	char	*path;
 
 	path = args[1];
-	if (ft_strcmp(path, "-"))
-		path = (find_in_env("OLDPWD") + 7);
-	printf("cd path: %s\n", path);
-	if (ft_doublestrlen(args) == 1 || check_dir(path))
+	if (path == NULL || !check_dir(path))
 		return ;
+	chdir(path);
 }
