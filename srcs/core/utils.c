8623357008411e055338894a98e36a9490584706
benatcastro <benatcastro@gmail.com>
2022-11-16 19:11:21 +0100

    env dupper fnc deleted

--- srcs/core/utils.c
+++ srcs/core/utils.c
@@ -6,23 +6,11 @@
 /*   By: becastro <becastro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/13 08:27:16 by becastro          #+#    #+#             */
-/*   Updated: 2022/11/16 19:03:59 by becastro         ###   ########.fr       */
+/*   Updated: 2022/11/16 19:11:06 by becastro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-
-void	env_dupper(char **env)
-{
-	int		i;
-
-	i = -1;
-	while (env[++i])
-	{
-		i++;
-		printf("%d", i);
-	}
-}
 
 void	print_double_str(char **str)
 {
