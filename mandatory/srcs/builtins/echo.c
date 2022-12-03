f6b5bf27d4a4bfe9b5a1d292e3bfe605197d27f1
Unai Martin marante <umartin-@c3r7s4.42urduliz.com>
2022-12-03 21:53:56 +0100

    echo -n full working

--- mandatory/srcs/builtins/echo.c
+++ mandatory/srcs/builtins/echo.c
@@ -6,11 +6,39 @@
 /*   By: umartin- <umartin-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/13 05:48:45 by becastro          #+#    #+#             */
-/*   Updated: 2022/12/03 17:31:50 by umartin-         ###   ########.fr       */
+/*   Updated: 2022/12/03 21:52:50 by umartin-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+int	first_arg_checker(char *buf)
+{
+	int	c;
+
+	c = 1;
+	if (buf[0] != '-' || buf[1] != 'n')
+		return (-1);
+	else
+	{
+		while (buf[++c])
+			if (buf[c] != 'n')
+				return (-1);
+	}
+	return (0);
+}
+
+void	echo_utils(char **cont, int i)
+{
+	while (cont[++i])
+	{
+		if (ft_strcmp(cont[i], ""))
+			continue ;
+		printf("%s", cont[i]);
+		if (cont[i + 1] != NULL)
+			printf(" ");
+	}
+}
 
 void	builtins_echo(char **cont)
 {
@@ -18,25 +46,15 @@ void	builtins_echo(char **cont)
 
 	if (!cont[1])
 		return ((void)printf("\n"));
-	if (cont[1][0] == '-' && cont[1][1] == 'n')
+	if (first_arg_checker(cont[1]) == 0)
 	{
 		i = 1;
-		while (cont[++i])
-		{
-			printf("%s", cont[i]);
-			if (cont[i + 1] != NULL)
-				printf(" ");
-		}
+		echo_utils(cont, i);
 	}
 	else
 	{
 		i = 0;
-		while (cont[++i])
-		{
-			printf("%s", cont[i]);
-			if (cont[i + 1] != NULL)
-				printf(" ");
-		}
+		echo_utils(cont, i);
 		printf("\n");
 	}
 }
